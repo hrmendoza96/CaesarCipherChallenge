5bb7dd6c8cf10c30c07a163988d6c2e4df273472
hrmendoza96 <hrmendoza96@unitec.edu>
2019-08-28 23:41:06 -0600

    Caesar Cipher Implemented

--- caesar_cipher/src/App.js
+++ caesar_cipher/src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Input from './components/Input';
 import TextList from './components/TextList';
 
@@ -6,15 +6,49 @@ import TextList from './components/TextList';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import uuid from 'uuid';
 
+
+
 function App() {
+  const [cipheredText, setCipheredTexts] = useState([
+    {
+      text: 'Hello Harold',
+    },
+    {
+      text: 'This is a test',
+    }
+  ]);
+
+  const cipher = (textToCipher) => {
+    const textArray = textToCipher.toUpperCase().split(""); //Splits each letter
+    const alphabet = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M",
+      "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+    let cipheredArray = [];
+    for (let i = 0; i < textArray.length; i++) {
+      if (alphabet.indexOf(textArray[i]) != -1) {
+        if (alphabet.indexOf(textArray[i]) <= 13) {
+          cipheredArray.push(alphabet[alphabet.indexOf(textArray[i]) + 13]);
+        } else {
+          cipheredArray.push(alphabet[alphabet.indexOf(textArray[i]) - 13]);
+        }
+      } else {
+        cipheredArray.push(textArray[i]);
+      }
+    }
+
+    
+    console.log(cipheredArray.join(""));
+  }
+
+
   return (
     <div>
       <div className="container">
         <div className="row">
           <div className="col col-md-8 mt-4 mx-auto ">
             <h2 className="text-capitalize text-center">The Caesar Cipher Challenge</h2>
+            {cipher("Hola Harold")}
             <Input/>
-            <TextList/>
+            <TextList />
           </div>
         </div>
       </div>
@@ -23,3 +57,6 @@ function App() {
 }
 
 export default App;
+
+
+
